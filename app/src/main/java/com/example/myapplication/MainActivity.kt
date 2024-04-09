f5ee31988aa72a40bde38d780190f48bd872a765
KacperPiatkowski <k.piatkowski07@gmail.com>
2024-04-09 13:52:02 +0200

    add player

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var resultTextView: TextView
     private lateinit var player1EditText: EditText
     private lateinit var player2EditText: EditText
+    private lateinit var historyTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +24,16 @@ class MainActivity : AppCompatActivity() {
         resultTextView = findViewById(R.id.resultTextView)
         player1EditText = findViewById(R.id.player1EditText)
         player2EditText = findViewById(R.id.player2EditText)
+        historyTextView = findViewById(R.id.historyTextView)
 
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener { playGame() }
 
         val showHistoryButton: Button = findViewById(R.id.showHistoryButton)
         showHistoryButton.setOnClickListener { showHistory() }
+
+        val addPlayerButton: Button = findViewById(R.id.addPlayerButton)
+        addPlayerButton.setOnClickListener { addPlayer() }
     }
 
     private fun playGame() {
@@ -56,7 +62,7 @@ class MainActivity : AppCompatActivity() {
     private fun showHistory() {
         val history = databaseManager.getAllResults()
         if (history.isEmpty()) {
-            resultTextView.text = "No game history available"
+            historyTextView.text = "No game history available"
             return
         }
 
@@ -64,11 +70,14 @@ class MainActivity : AppCompatActivity() {
         for (gameResult in history) {
             historyString.append("Player: ${gameResult.player}, Score: ${gameResult.score}\n")
         }
-        resultTextView.text = historyString.toString()
+        historyTextView.text = historyString.toString()
+    }
+
+    private fun addPlayer() {
+        // Tutaj możesz dodać kod obsługujący dodawanie nowego użytkownika
     }
 
     private fun rollDice(): Int {
         return Random.nextInt(1, 7) // Losujemy liczbę od 1 do 6 (jak w standardowej kostce)
     }
 }
-
